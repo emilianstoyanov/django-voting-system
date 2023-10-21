516d8a349243b7d2855ed33c9c0ad4db55edb82b
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-21 15:18:31 +0300

    add vote view

--- pollProject/pollApp/views.py
+++ pollProject/pollApp/views.py
@@ -19,7 +19,7 @@ def index(request):
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
-    except ObjectDoesNotExist:
+    except Question.DoesNotExist:
         raise Http404('Question does not exist')
     return render(request, 'polls / details.html', {'question': question})
 
@@ -30,3 +30,17 @@ def results(request, question_id):
     return render(request, 'polls / results.html', {'question': question})
 
 
+# Vote for a question choice
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,
+                      'polls / detail.html',
+                      {'question': question,
+                       'error_message': 'You did not select a choice.'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
