70096ad597456a3ed4caeb23ae94917ef5a9cd08
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-21 12:57:22 +0300

    add ChoiseLine and QuesntionAdmin classes in admin

--- pollProject/pollApp/admin.py
+++ pollProject/pollApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+admin.site.site_header = "The Poll Mall"
+admin.site.site_header = "Voting Admin Area"
+admin.site.site_header = "Welcome to our Voting Admin Area"
+
+
+class ChoiceLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceLine]
+
+
+admin.site.register(Question, QuestionAdmin)
