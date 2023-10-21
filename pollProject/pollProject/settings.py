2d43ec4e826f44dc6cd997230615f8e60f1b1ca6
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-21 12:29:57 +0300

    setting up the PostgreSQL database

--- pollProject/pollProject/settings.py
+++ pollProject/pollProject/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'pollProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('DATABASES_NAME'),
+        "USER": config('DATABASES_USER'),
+        "PASSWORD": config('DATABASES_PASSWORD'),
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
