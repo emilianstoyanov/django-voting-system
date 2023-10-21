581f00cdca0f55ea7e3e841003b8c695b8c48d80
Emilian Stoyanov <emilian.stoyanov@outlook.com>
2023-10-21 15:05:06 +0300

    add results view

--- pollProject/pollApp/views.py
+++ pollProject/pollApp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Question, Choice
 
@@ -11,13 +12,21 @@ from .models import Question, Choice
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls / index.html', context)
 
 
 # Show question and choices
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
+    except ObjectDoesNotExist:
         raise Http404('Question does not exist')
     return render(request, 'polls / details.html', {'question': question})
+
+
+# Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls / results.html', {'question': question})
+
+
